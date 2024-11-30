b21e8907e1327bc6c3b9105a84d43c4a84e0a439
Aditya Pant <pantanupam05@gmail.com>
2024-11-30 16:13:44 +0530

    checkForDay

--- src/switchStatement.java
+++ src/switchStatement.java
@@ -1,5 +1,31 @@
 public class switchStatement {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        checkForDay(5);
+    }
+    public static void checkForDay(int day) {
+
+    switch (day){
+        case 1 :
+            System.out.println("monday");
+            break;
+        case 2:
+            System.out.println("tuesday");
+            break;
+        case 3:
+            System.out.println("wednesday");
+            break;
+        case 4:
+            System.out.println("thursday");
+            break;
+        case 5:
+            System.out.println("friday");
+            break;
+        case 6:
+            System.out.println("saturday");
+            break;
+        case 7:
+            System.out.println("sunday");
+            break;
+    }
     }
 }
