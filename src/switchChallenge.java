69d7d8dad073cf40046e797ae9bf690d2fc6178b
Aditya Pant <pantanupam05@gmail.com>
2024-11-30 17:50:55 +0530

    switch statement another chalenge

---
+++ src/switchChallenge.java
@@ -0,0 +1,34 @@
+public class switchChallenge {
+    public static void main(String[] args){
+        nameNato('E');
+    }
+    public static void nameNato ( char nato){
+
+        switch (nato){
+            case 'A':
+                System.out.println("A is able");
+                break;
+            case 'B':
+                System.out.println("B is baker");
+                break;
+            case 'C':
+                System.out.println("C is charlie");
+                break;
+            case 'D':
+                System.out.println("D is dog");
+                break;
+            case 'E':
+                System.out.println("E is easy");
+                break;
+            default:
+                System.out.println("enter a valid range A-E");
+
+//            case 'A' -> System.out.println("Able");
+//            case 'B' -> System.out.println("baker");
+//            case 'C' -> System.out.println("Charlie");
+//            case 'D' -> System.out.println("Dog");
+//            case 'E' ->System.out.println("Easy");
+//            default -> System.out.println("enter a valid range between 1 to 5 ");
+        }
+    }
+}
