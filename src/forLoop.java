648f45a7866f8d28b1848ef224b5a841cda665cb
Aditya Pant <pantanupam05@gmail.com>
2024-12-01 21:36:26 +0530

    interest rates

--- src/forLoop.java
+++ src/forLoop.java
@@ -3,5 +3,9 @@ public class forLoop {
         for(int i =1; i <5 ; i++){
             System.out.println(i);
         }
+        System.out.println("10,000 at 2% interest is " + returnValue(2, 10000));
+    }
+    public static double returnValue(double interest, double amount){
+        return ( amount*(interest/100));
     }
 }
