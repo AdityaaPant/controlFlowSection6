60cea677e180b1369e004e20b48b5635f936e960
Aditya Pant <pantanupam05@gmail.com>
2024-12-02 21:30:25 +0530

    basic

---
+++ src/isPrime.java
@@ -0,0 +1,13 @@
+public class isPrime {
+    public static void main(String[] args){
+        System.out.println("0 is " + (isPrime(0)? "" :"NOt")+ "a prime number");
+        System.out.println("2 is " + (isPrime(2)? "" :"NOt")+ "a prime number");
+        System.out.println("1 is " + (isPrime(1)? "" :"NOt")+ "a prime number");
+        System.out.println("3 is " + (isPrime(3)? "" :"NOt")+ "a prime number");
+    }
+    public static boolean isPrime(int wholeNumber){
+        if(wholeNumber<=2){
+            
+        }
+    }
+}
