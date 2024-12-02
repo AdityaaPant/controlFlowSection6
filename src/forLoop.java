39cd23c69d7c28951207a6ea8d81148f7754ccd4
Aditya Pant <pantanupam05@gmail.com>
2024-12-02 21:21:18 +0530

    initial

--- src/forLoop.java
+++ src/forLoop.java
@@ -1,13 +1,17 @@
 public class forLoop {
+
     public static void main(String[] args){
-        for(int i =1; i <5 ; i++){
-            System.out.println(i);
-        }
-        for(double rate = 2.0 ; rate <= 5 ; rate++){
-            double interestAmount = returnValue(rate, 10000);
-        System.out.println("10,000 at "+ rate +" interest is " + interestAmount);
+        for( double interestRate= 7.5; interestRate<=10; interestRate+=0.25 ){
+            double intAmount = returnValue(interestRate, 100);
+            System.out.println("$100 at " + interestRate +" interest is " +intAmount);
         }
     }
+//    public static void main(String[] args){
+//        for(double rate = 2.0 ; rate <= 5 ; rate++){
+//            double interestAmount = returnValue(rate, 10000);
+//            System.out.println("10,000 at "+ rate +" interest is " + interestAmount);
+//        }
+//    }
     public static double returnValue(double interest, double amount){
         return ( amount*(interest/100));
     }
