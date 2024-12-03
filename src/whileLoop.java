c1ef2e708320458f3629d2f82e908ae9afb3846a
Aditya Pant <pantanupam05@gmail.com>
2024-12-03 22:06:39 +0530

    final push of the day

--- src/whileLoop.java
+++ src/whileLoop.java
@@ -1,11 +1,39 @@
 public class whileLoop {
     public static void main(String[] args){
-        int j =0;
+        int number = 4 ;
+        int finishNumber = 20;
+        int count = 0;
+        int oddcount = 0;
+        int evencount = 0;
+        while( number <= finishNumber){
+            number++;
 
-        while( j <= 50 ){
+            if(!isEvenNumber(number)){
+                oddcount++;
+                continue;
+            }
+            System.out.println("Even number " + number );
+            evencount ++;
 
-            System.out.println("the number is " + j);
-            j+=5;
+
+            if(count ==4){
+                break;
+            }else{
+                count++;
+            }
+        }
+        System.out.println("number of odd numbers is " + oddcount);
+        System.out.println("number of even numbers is " + evencount);
+        System.out.println("number count is "+ count);
+
+
+    }
+    public static boolean isEvenNumber(int number){
+            if(number %2 ==0){
+                return true;
+            } else {
+                return false;
+            }
         }
     }
-}
+
