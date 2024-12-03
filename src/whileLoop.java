92234e98f840212ce2f3334dc3f817a0c3ded558
Aditya Pant <pantanupam05@gmail.com>
2024-12-03 21:44:53 +0530

    while loop

---
+++ src/whileLoop.java
@@ -0,0 +1,11 @@
+public class whileLoop {
+    public static void main(String[] args){
+        int j =0;
+
+        while( j <= 50 ){
+
+            System.out.println("the number is " + j);
+            j+=5;
+        }
+    }
+}
