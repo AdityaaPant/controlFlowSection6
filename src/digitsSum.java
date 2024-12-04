1f75689727dfb1dd86730852392607ba602b5d1f
Aditya Pant <pantanupam05@gmail.com>
2024-12-04 19:43:30 +0530

    initial commit

---
+++ src/digitsSum.java
@@ -0,0 +1,18 @@
+public class digitsSum {
+    public static void main(String[] args){
+        System.out.println("the sum is " + sumDigits(2112));
+    }
+    public static int sumDigits(int number){
+        if(number < 0){
+            return -1;
+        }
+        int sum =0;
+        while(number > 9) {
+            sum += (number % 10);
+            number = number /10;
+
+        }
+        sum += number;
+        return sum;
+    }
+}
