426e39add51f64dc93a176a2dbfd805820a470ca
Aditya Pant <pantanupam05@gmail.com>
2024-12-05 15:10:16 +0530

    initial commit

--- src/digitsSum.java
+++ src/digitsSum.java
@@ -1,16 +1,15 @@
 public class digitsSum {
     public static void main(String[] args){
-        System.out.println("the sum is " + sumDigits(2112));
+        System.out.println("the sum of digits in the number is " + sumDigits(2112) );
     }
     public static int sumDigits(int number){
-        if(number < 0){
+        if  (number < 0 ){
             return -1;
         }
         int sum =0;
-        while(number > 9) {
-            sum += (number % 10);
+        while (number > 9){
+            sum += (number %10 ) ;
             number = number /10;
-
         }
         sum += number;
         return sum;
