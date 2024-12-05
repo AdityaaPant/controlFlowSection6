cc531048562e180162744542432254b4f23831bf
Aditya Pant <pantanupam05@gmail.com>
2024-12-05 20:06:10 +0530

    first try with palindrome

---
+++ src/NumberPalindrome.java
@@ -0,0 +1,19 @@
+public class NumberPalindrome {
+    public static void main(String[] args){
+            System.out.println("the number having the palindrome is" + isPalindrome(212));
+    }
+    public static boolean isPalindrome(int number){
+          int reverse=0;
+          int lastDigit=0;
+          if(reverse == number){
+              while(number>=0){
+                  lastDigit = number % 10;
+                  reverse = reverse*10;
+                  
+              }
+          }else{
+              return false;
+          }
+
+    }
+}
