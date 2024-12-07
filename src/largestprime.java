6c74155b6921874c619b9e71046736491acfb204
Aditya Pant <pantanupam05@gmail.com>
2024-12-07 19:46:31 +0530

    largest prime

--- src/largestprime.java
+++ src/largestprime.java
@@ -1,3 +1,25 @@
 public class largestprime {
-    
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(23));
+    }
+
+    public static int getLargestPrime(int number) {
+        if (number < 2) {
+            return -1;
+        }
+        int factor = -1;
+        if (number > 10) {
+            for (int i = 2; i * i <= number; i++) {
+                if (number % i != 0) {
+                    continue;
+                }
+                factor = i;
+                while (number % i == 0) {
+                    number /= i;
+
+                }
+            }
+
+        }return number == 1 ? factor : number;
+    }
 }
