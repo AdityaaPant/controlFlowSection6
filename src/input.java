e962958388bc83ef7000f5e963bb9fc136c61997
Aditya Pant <pantanupam05@gmail.com>
2024-12-09 21:50:35 +0530

    validations

--- src/input.java
+++ src/input.java
@@ -19,15 +19,30 @@ public class input {
             System.out.println("hi " + name + " thanks for coding");
 
             System.out.println("what year you were born ");
-            String dateOfBirth= scanner.nextLine();
-            int age = currentYear - Integer.parseInt(dateOfBirth);
 
+            boolean validDOB= false;
+            int age =0;
+
+            do {
+                System.out.println("Enter a year of birth >= "+ (currentYear -125)+ " and <= " + (currentYear));
+               try{
+                   age = checkData(currentYear , scanner.nextLine());
+                   validDOB =age < 0 ? false:true;
+               }catch(NumberFormatException badUserData){
+                   System.out.println("Characters not allowed!! try again..");jj
+               }
+
+            }while(!validDOB);
             return "so you are " + age + " years old";
 
         }
     public static int checkData(int currentYear , String dateOfBirth){
-        int dob = Integer.parseInt(dateOfBirth)
-;
-    int minimumYear}
+        int dob = Integer.parseInt(dateOfBirth);
+    int minimumYear = currentYear-125;
+    if((dob<minimumYear)|| (dob >currentYear )){
+        return -1;
+    }
+    return (currentYear-dob);
+    }
 
 }
